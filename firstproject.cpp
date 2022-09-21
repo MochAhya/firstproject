9c3a158733e5dbb227bc5b47f5600f173cb8d428
Mochamad Ahya Satari <111238362+MochAhya@users.noreply.github.com>
2022-09-21 21:02:54 +0700

    Add files via upload

---
+++ firstproject.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float ipk;
+    cout <<"Masukkan IPK anda =="<<flush;
+    cin >> ipk;
+    cout << "Nilai IPK anda adalah = "<<ipk << "(apik tenan)";
+    
+    return 0;
+}
